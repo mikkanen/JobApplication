82f3ce5c70a6ccde2d2046f3b604a3a015c28a07
mikkanen <markku.mikkanen@saunalahti.fi>
2026-05-12 17:36:12 +0300

    Corrections

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rsync -avz --exclude 'build/' --exclude '.git/' ~/Development/hue_jobapplication/ mikkanen@Raspberry5.local:~/Development/hue_jobapplication/
-# ssh mikkanen@Raspberry5.local "cd ~/Development/hue_jobapplication/build && make -j$(nproc)"
+rsync -avz --exclude 'build/' --exclude '.git/' ~/Development/jobapplication/ mikkanen@Raspberry5.local:~/Development/jobapplication/
+# ssh mikkanen@Raspberry5.local "cd ~/Development/jobapplication/build && make -j$(nproc)"
 
