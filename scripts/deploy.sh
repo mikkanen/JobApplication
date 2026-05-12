600279a0719b6da08d3a82d26c9a8731ed021ead
mikkanen <markku.mikkanen@saunalahti.fi>
2026-05-12 17:25:15 +0300

    File structure update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-rsync -avz --exclude 'build/' --exclude '.git/' ~/Development/hue_alarmclock/ mikkanen@Raspberry5.local:~/Development/hue_alarmclock/
-# ssh mikkanen@Raspberry5.local "cd ~/Development/hue_alarmclock/build && make -j$(nproc)"
+rsync -avz --exclude 'build/' --exclude '.git/' ~/Development/hue_jobapplication/ mikkanen@Raspberry5.local:~/Development/hue_jobapplication/
+# ssh mikkanen@Raspberry5.local "cd ~/Development/hue_jobapplication/build && make -j$(nproc)"
+
